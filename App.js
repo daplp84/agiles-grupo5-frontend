38cd9c3b03c0af8b6e13127af7058e78bb25ed55
Daniel Piassini <danielpiassini@hotmail.com>
2021-09-13 19:46:08 -0300

    Arreglo estructura de Front

--- App.js
+++ App.js
@@ -1,48 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Header } from 'react-native';
+import Main from './screens/main';
 
 export default function App() {
   return (
+    <Main />
     
-    <View style={styles.container}>
-     <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-      <Text>Bienvendo</Text>
-      <TextInput style={styles.input} placeholder="Usuario"></TextInput>
-      <TextInput style={styles.input} placeholder="Contraseña"></TextInput>
-      <StatusBar style="auto" />
-    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  input: {
-    height: 40,
-    width: 300,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-
-  logo: {
-    width: 66,
-    height: 58,
-  },
-
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-});
