3eb7d85c23e65d529bbc218bc66d9ca04b21492c
Daniel Piassini <danielpiassini@hotmail.com>
2021-11-11 16:23:23 -0300

    Agrega bar de prueba

--- screens/qrScanner.jsx
+++ screens/qrScanner.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useState, useContext, useEffect } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import OrderContext from "../contexts/orderContext";
+import BarContext from '../contexts/barContext';
 import { Camera } from 'expo-camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 const QRScanner = () => {
+    const navigation = useNavigation();
+    const { setCurrentOrder } = useContext(OrderContext);
+    const { setCurrentBar } = useContext(BarContext);
     const [hasPermission, setHasPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
     const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
+    const [scanned, setScanned] = useState(false);
 
     useEffect(() => {
         (async () => {
@@ -26,13 +31,30 @@ const QRScanner = () => {
     }
 
     const onBarCodeScanned = (data) => {
-        alert("Escaneamos un código: " + data.type  + " con el dato: " + data.data);
+        setScanned(true);
+        const qrContent = JSON.parse(data.data);
+        if(qrContent.App == "BirraYa"){
+            const bar = qrContent.BarId;
+            setCurrentBar(bar);
+            setCurrentOrder("1", bar);
+            navigation.push("menuBar");
+        }else{
+            setScanned(false);
+        }
+        
     }
   
     return(
         <View style={styles.container}>
-            <Camera style={styles.camera} type={type} flashMode={flashMode} onBarCodeScanned={onBarCodeScanned} barCodeScannerSettings={{ barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr]}}>
-
+            <Camera style={styles.camera} type={type} flashMode={flashMode} onBarCodeScanned={scanned ? null : onBarCodeScanned} barCodeScannerSettings={{ barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr]}}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={() => {setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);}}>
+                        <Image style={styles.image} source={require('../images/flip.png')}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => setFlashMode(flashMode === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.torch : Camera.Constants.FlashMode.off)}>
+                        <Image style={styles.image} source={ flashMode === Camera.Constants.FlashMode.off ? require('../images/led-off.png') : require('../images/led-lamp.png')}></Image>
+                    </TouchableOpacity>
+                </View>
             </Camera>
         </View>
     );
@@ -44,7 +66,27 @@ const styles = StyleSheet.create({
     },
     camera: {
       flex: 1,
-    }
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        margin: 20,
+        width:  '150%'
+      },
+      button: {
+        flex: 0.1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+      },
+      text: {
+        fontSize: 18,
+        color: 'white',
+      },
+      image: {
+          width: 50,
+          height: 50
+      }
 });
 
 export default QRScanner;
